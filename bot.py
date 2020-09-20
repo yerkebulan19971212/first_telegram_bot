3a1bdb161dfaed2b44dc86cd70491c5c7b2286e6
yerkebulan19971212 <erkebulan19971212@gmail.com>
2020-09-21 00:49:24 +0600

    wellcom

--- bot.py
+++ bot.py
@@ -4,6 +4,14 @@ import config
 bot = telebot.TeleBot(config.TOKEN)
 
 
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(
+        message.chat.id,
+        "Wellcome",
+    )
+
+
 @bot.message_handler(content_types=['text'])
 def lalala(message):
     bot.send_message(message.chat.id, message.text)
