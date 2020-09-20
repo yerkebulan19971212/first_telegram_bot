c12ae3a90ebf2f7df9ede97105184bfb0254e464
yerkebulan19971212 <erkebulan19971212@gmail.com>
2020-09-21 00:40:09 +0600

    first telegramBot

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+# run
+bot.polling(none_stop=True)
